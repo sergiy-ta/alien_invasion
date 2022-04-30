36fe85a08500f7525c1b0cd463ff72667a71bc4f
Serhii Tarliev <s.tarliev@avidi.tech>
2022-04-30 20:25:02 +0300

    added bullet

--- settings.py
+++ settings.py
@@ -5,3 +5,6 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
--- ship.py
+++ ship.py
@@ -8,7 +8,7 @@ class Ship:
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load('image/ship.bmp')
+        self.image = pygame.image.load('image/ship-small.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
